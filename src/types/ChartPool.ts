824e880c261982c4d250e40e440e73bb14f398ed
alancooper509 <alancooper509@hotmail.com>
2026-02-05 12:05:50 -0600

    matching supabase table definition update

--- src/types/ChartPool.ts
+++ src/types/ChartPool.ts
@@ -4,6 +4,7 @@ export interface ChartPool {
   id: number;               // bigint -> number
   stage_id: number;         // bigint -> number
   chart_id?: number;        // bigint -> number
+  difficulties: Text;       // JSON string representing an array of difficulty levels
   created_at: string;       // ISO timestamp string
   charts: Chart | null;
 }
