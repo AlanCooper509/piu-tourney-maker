a1aa6a68d35fb0d3766d0f8a1c2df9cb42dc4611
alancooper509 <alancooper509@hotmail.com>
2026-07-01 16:41:13 -0500

    carrying updates from ChartRulesCard (using context instead of drilling)

--- src/components/rulesets/ChartRulesList.tsx
+++ src/components/rulesets/ChartRulesList.tsx
@@ -76,8 +76,6 @@ export default function ChartRulesList({
               pickbanRulesets={pickbanRulesets}
               roundPools={roundPools}
               setRoundPools={setRoundPools}
-              isTourneyAdmin={isTourneyAdmin}
-              loadingTourneyAdminStatus={loadingTourneyAdminStatus}
             />
           ))}
       </Stack>
